4963f1eb75dd2f8ece5a00d1ae0d1c02b22cf096
Nikolay111906 <127125040+Nikolay111906@users.noreply.github.com>
2024-05-09 22:56:28 +0300

    Devided the code into functions

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,31 +1,41 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
+void readArray(int arr[], int size) {
+	cout << "Enter " << size << " numbers to fill the array: ";
+	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+	}
+}
+
+void sortArray(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+
+void printArray(const int arr[], int size) {
+	cout << "Array in sorted form: ";
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
 int main() {
-    int digitals[10];
+	const int SIZE = 10;
+	int digitals[SIZE];
 
-    cout << "Enter 10 numbers to fill the array: ";
+	readArray(digitals, SIZE);
+	sortArray(digitals, SIZE);
+	printArray(digitals, SIZE);
 
-    for (int i = 0; i < 10; i++) {
-        cin >> digitals[i];
-    }
-
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 9; j++) {
-            if (digitals[j] > digitals[j + 1]) {
-                int b = digitals[j];
-                digitals[j] = digitals[j + 1];
-                digitals[j + 1] = b;
-            }
-        }
-    }
-
-    cout << "An array in sorted form: ";
-
-    for (int i = 0; i < 10; i++) {
-        cout << digitals[i] << " ";
-    }
-
-    return 0;
+	return 0;
 }
